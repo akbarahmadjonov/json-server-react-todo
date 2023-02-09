c140717754eef839edb0fdd08e6788ce26ac03aa
akbarahmadjonov <akhmadjonovakbardeveloper@gmail.com>
2023-02-09 13:46:37 +0500

    Add add/remove/edit features to todolist

--- src/Components/Form/List.jsx
+++ src/Components/Form/List.jsx
@@ -1,31 +1,58 @@
-import { useState } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { useRef } from "react";
-import Item from "../Item";
+import "./Form.css";
 
 const Form = () => {
-  const [todos, setTodos] = useState(
-    JSON.parse(localStorage.getItem("todos")) || []
-  );
+  const [data, setData] = useState([]);
+
   let elInput = useRef();
+  let elCheckbox = useRef();
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:8080/todos")
+      .then((data) => {
+        if (data.status === 200) {
+          setData(data.data);
+        }
+      })
+      .catch((err) => console.log(err));
+  }, [data]);
 
   const handleSubmit = (evt) => {
     evt.preventDefault();
-    if (!elInput.current.value) {
+    axios
+      .post("http://localhost:8080/todos", {
+        title: elInput.current.value,
+      })
+      .then((data) => console.log(data));
+  };
+
+  const handleDelete = (id) => {
+    // Delete by ID, from button edit
+    axios
+      .delete(`http://localhost:8080/todos/${id}`)
+      .then((data) => console.log(data))
+      .catch((err) => console.log(err));
+  };
+
+  const handleEdit = (id) => {
+    const newPrompt = prompt(
+      "You would like to change your todo in the following ID?",
+      id
+    );
+    // If prompt value is false return false
+    if (!newPrompt) {
       return false;
     }
 
-    setTodos([
-      ...todos,
-      {
-        id: todos.at(-1)?.id + 1 || 1,
-        isCompleted: false,
-        text: elInput.current.value,
-      },
-    ]);
-    elInput.current.value = "";
+    const edited = { title: newPrompt };
+    axios
+      .put(`http://localhost:8080/todos/${id}`, edited)
+      .then((data) => console.log(data))
+      .catch((err) => console.log(err));
   };
-
-  localStorage.setItem("todos", JSON.stringify(todos));
 
   return (
     <div className="container">
@@ -33,18 +60,18 @@ const Form = () => {
       <div className="row flex-column">
         <form
           onSubmit={handleSubmit}
-          className="col-12 col-lg-6 text-center mt-5 mx-auto rounded js-form"
+          className="col-12 col-lg-6 text-center mt-5 mx-auto rounded js-form m-0 p-0"
         >
           <div>
             <h1 className="display-3 fw-bold mb-3">Todo App</h1>
           </div>
-          <div className="input-group">
+          <div className="input-group w-100">
             <input
               ref={elInput}
               className="form-control js-input"
               type="text"
               placeholder="Enter any task"
-              autoFocus="true"
+              autoFocus={true}
             />
             <button className="btn btn-primary p-3">
               <i className="fas fa-plus-circle fs-3"></i>
@@ -54,17 +81,43 @@ const Form = () => {
             Enter a text greater than 3 words
           </span>
         </form>
-        <ul className="w-50 mx-auto list-group col-12 col-lg-12 mt-5 list-group mx-auto">
-          {todos.map((todo) => (
-            <Item
-              id={todo.id}
-              isCompleted={todo.isCompleted}
-              text={todo.text}
-              todos={todos}
-              setTodos={setTodos}
-            />
-          ))}
-        </ul>
+        {data.length ? (
+          <ul className="w-50 mx-auto list-group col-12 col-lg-12 mt-5 list-group p-0 m-0">
+            {data.map((todo) => (
+              <li
+                key={todo.id}
+                className="list-group-item d-flex align-items-center p-2"
+              >
+                <strong>{todo.id}</strong>.{" "}
+                <input
+                  className="ms-2 me-2 form-check-input check"
+                  type="checkbox"
+                  name="checkbox"
+                />
+                <span
+                  onClick={() => handleEdit(todo.id)}
+                  className="ms-2 bg-light p-1 rounded span"
+                >
+                  {todo.title}
+                </span>
+                <button
+                  onClick={() => handleEdit(todo.id)}
+                  className="ms-auto me-2 btn btn-secondary"
+                >
+                  EDIT
+                </button>
+                <button
+                  onClick={() => handleDelete(todo.id)}
+                  className="btn btn-danger"
+                >
+                  DELETE
+                </button>
+              </li>
+            ))}
+          </ul>
+        ) : (
+          ""
+        )}
       </div>
     </div>
   );
